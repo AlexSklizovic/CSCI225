5d61e7ef06f223beff9a19dc01e37d66719da04f
Alex Sklizovic <sklizo44@gmail.com>
2020-10-21 23:31:54 -0400

    calc trial

---
+++ js/conversion.js
@@ -0,0 +1,76 @@
+let inVal = document.getElementById("input").value;
+            
+            switch(document.getElementById("length1").value + "|" + document.getElementById("length2").value){
+                case "Inches|Inches":
+                    break;
+                case "Inches|Feet":
+                    break;
+                case "Inches|Yards":
+                    break;
+                case "Inches|Millimeters":
+                    break;
+                case "Inches|Centimeters":
+                    break;
+                case "Inches|Meters":
+                    break;
+                case "Feet|Inches":
+                    break;
+                case "Feet|Feet":
+                    break;
+                case "Feet|Yards":
+                    break;
+                case "Feet|Millimeters":
+                    break;
+                case "Feet|Centimeters":
+                    break;
+                case "Feet|Meters":
+                    break;
+                case "Yards|Inches":
+                    break;
+                case "Yards|Feet":
+                    break;
+                case "Yards|Yards":
+                    break;
+                case "Yards|Millimeters":
+                    break;
+                case "Yards|Centimeters":
+                    break;
+                case "Yards|Meters":
+                    break;
+                case "Millimeters|Inches":
+                    break;
+                case "Millimeters|Feet":
+                    break;
+                case "Millimeters|Yards":
+                    break;
+                case "Millimeters|Millimeters":
+                    break;
+                case "Millimeters|Centimeters":
+                    break;
+                case "Millimeters|Meters":
+                    break;
+                case "Centimeters|Inches":
+                    break;
+                case "Centimeters|Feet":
+                    break;
+                case "Centimeters|Yards":
+                    break;
+                case "Centimeters|Millimeters":
+                    break;
+                case "Centimeters|Centimeters":
+                    break;
+                case "Centimeters|Meters":
+                    break;
+                case "Meters|Inches":
+                    break;
+                case "Meters|Feet":
+                    break;
+                case "Meters|Yards":
+                    break;
+                case "Meters|Millimeters":
+                    break;
+                case "Meters|Centimeters":
+                    break;
+                case "Meters|Meters":
+                    break;
+            }
